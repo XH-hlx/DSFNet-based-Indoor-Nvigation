437b0e91a6e6dffd5d9c046c587cbc5224968b4e
XH-hlx <2221339687@qq.com>
2025-11-22 14:20:44 +0800

    DSFNet

---
+++ DSFNet/config.py
@@ -0,0 +1,46 @@
+import numpy as np
+import torch
+
+class Path(object):
+    @staticmethod
+    def db_root_dir(dataset):
+        if dataset == 'nyu':
+            # folder that contains dataset/.
+            return {'train': '/data/common_datasets/NYU_SSC/NYUtrain_npz_all',
+                    'val': '/data/common_datasets/NYU_SSC/NYUtest_npz_all'}
+
+        elif dataset == 'nyucad':
+            return {'train': '/data/common_datasets/NYU_SSC/NYUCADtrain_npz_all',
+                    'val': '/data/common_datasets/NYU_SSC/NYUCADtest_npz_all'}
+
+        # debug
+        elif dataset == 'debug':
+            return {'train': '/data/jenny/NYU_SSC/NYUCADval40_npz',
+                    'val': '/data/jenny/NYU_SSC/NYUCADval40_npz'}
+        else:
+            print('Dataset {} not available.'.format(dataset))
+            raise NotImplementedError
+
+
+# ssc: color map
+colorMap = np.array([[22, 191, 206],    # 0 empty, free space
+                     [214,  38, 40],    # 1 ceiling
+                     [43, 160, 4],      # 2 floor
+                     [158, 216, 229],   # 3 wall
+                     [114, 158, 206],   # 4 window
+                     [204, 204, 91],    # 5 chair  new: 180, 220, 90
+                     [255, 186, 119],   # 6 bed
+                     [147, 102, 188],   # 7 sofa
+                     [30, 119, 181],    # 8 table
+                     [188, 188, 33],    # 9 tvs
+                     [255, 127, 12],    # 10 furn
+                     [196, 175, 214],   # 11 objects
+                     [153, 153, 153],     # 12 Accessible area, or label==255, ignore
+                     ]).astype(np.int32)
+
+# ###########################################################################################
+
+class_weights = torch.FloatTensor([0.4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])  # changed
+
+
+

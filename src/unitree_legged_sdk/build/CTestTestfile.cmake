6e805e21c9152e8d6a10e6a0d77df9bfe6700a63
XH-hlx <2221339687@qq.com>
2025-11-22 13:31:18 +0800

    re

--- src/unitree_legged_sdk/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/shuwake/robot_ws/src/unitree_legged_sdk
-# Build directory: /home/shuwake/robot_ws/src/unitree_legged_sdk/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("python_wrapper")
-subdirs("gtest")

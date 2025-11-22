8f8b6a9d66f551e8f8f029efc87df9a90665b903
XH-hlx <2221339687@qq.com>
2025-11-22 14:54:22 +0800

    control

---
+++ src/unitree_legged_sdk/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/shuwake/robot_ws/src/unitree_legged_sdk
+# Build directory: /home/shuwake/robot_ws/src/unitree_legged_sdk/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("python_wrapper")
+subdirs("gtest")
---
+++ src/unitree_legged_sdk/build/python_wrapper/CMakeFiles/robot_interface.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../../lib/python/amd64/robot_interface.cpython-38-x86_64-linux-gnu.so"
+  "../../lib/python/amd64/robot_interface.pdb"
+  "CMakeFiles/robot_interface.dir/python_interface.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/robot_interface.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
